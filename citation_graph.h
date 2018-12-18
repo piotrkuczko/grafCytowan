59651dadb4525d045e654369631cdac862c5eb62
Piotr Kuczko <pk394397@students.mimuw.edu.pl>
2018-12-18 13:28:18 +0100

    Exceptions added

--- citation_graph.h
+++ citation_graph.h
@@ -2,6 +2,9 @@
 #define GRAFCYTOWAN_CITATION_GRAPH_H
 
 #include <vector>
+#include <memory>
+#include <set>
+#include <map>
 
 
 struct PublicationNotFound : public std::exception {
@@ -26,23 +29,46 @@ struct TriedToRemoveRoot : public std::exception {
 
 template <class Publication>
 class CitationGraph {
+
+private:
+    class Node {
+    public:
+        Publication id;
+        bool isRoot;
+        std::set<std::shared_ptr<Node> > children;
+        std::set<std::weak_ptr<Node> > parents;
+        Node (Publication::id_type const &stem_id, bool root)
+           : id(stem_id), isRoot(root) {}
+    };
+    Node *root;
+    std::map <Publication, std::weak_ptr> map;
+public:
     // Tworzy nowy graf. Tworzy także węzeł publikacji o identyfikatorze stem_id.
-    CitationGraph(Publication::id_type const &stem_id);
+    CitationGraph(Publication::id_type const &stem_id) {
+        root = new Node(stem_id, true);
+    }
 
 // Konstruktor przenoszący i przenoszący operator przypisania. Powinny być
 // noexcept.
-    CitationGraph(CitationGraph<Publication> &&other);
-    CitationGraph<Publication>& operator=(CitationGraph<Publication> &&other);
+    CitationGraph(CitationGraph<Publication> &&other) noexcept {
+        this = std::move(other);
+    }
+    CitationGraph<Publication>& operator=(CitationGraph<Publication> &&other) {
+        this = std::move(other);
+    }
 
 // Zwraca identyfikator źródła. Metoda ta powinna być noexcept wtedy i tylko
 // wtedy, gdy metoda Publication::get_id jest noexcept. Zamiast pytajnika należy
 // wpisać stosowne wyrażenie.
-    Publication::id_type get_root_id() const noexcept(?);
+        // https://akrzemi1.wordpress.com/2011/06/10/using-noexcept/
+    Publication::id_type get_root_id() const noexcept(is_nothrow_move_constructible<T>::value && is_nothrow_move_assignable<T>::value);
 
 // Zwraca listę identyfikatorów publikacji cytujących publikację o podanym
 // identyfikatorze. Zgłasza wyjątek PublicationNotFound, jeśli dana publikacja
 // nie istnieje.
-    std::vector<Publication::id_type> get_children(Publication::id_type const &id) const;
+    std::vector<Publication::id_type> get_children(Publication::id_type const &id) const {
+
+    };
 
 // Zwraca listę identyfikatorów publikacji cytowanych przez publikację o podanym
 // identyfikatorze. Zgłasza wyjątek PublicationNotFound, jeśli dana publikacja
