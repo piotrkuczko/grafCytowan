32a0901002a10fc130b8ac2e6355ba756fda5381
Michał Łopata <ml394432@students.mimuw.edu.pl>
2018-12-19 13:13:51 +0100

    zmiany dla Kuczka

--- citation_graph.h
+++ citation_graph.h
@@ -32,20 +32,21 @@ class CitationGraph {
 
 private:
     class Node {
-    public:
-        Publication::id_type id;
-        bool isRoot;
-        std::set<std::shared_ptr<Node> > children;      // właściwie to czemu tu nie weak_ptr tylko shared_ptr??
-        std::set<std::weak_ptr<Node> > parents;         // BBBudro: "można stracić punkty za trzymanie
-        Node (Publication::id_type const &stem_id, bool root)   // internalsów publicznie", to chyba do tego sie odnosi
-           : id(stem_id), isRoot(root) {}
+    private:
+        Publication publication;
+        std::set<std::shared_ptr<Node> > children;
+        std::set<std::weak_ptr<Node> > parents;
+        Node (typename Publication::id_type const &stem_id)
+                : publication(stem_id) {}
     };
+
     std::shared_ptr<Node> root;
-    std::map <Publication::id_type, std::weak_ptr<Node>> map;
+    std::map <typename Publication::id_type, std::weak_ptr<Node>> map;
+
 public:
     // Tworzy nowy graf. Tworzy także węzeł publikacji o identyfikatorze stem_id.
-    CitationGraph(Publication::id_type const &stem_id) {
-        root = std::make_shared<Node>(stem_id, true);
+    CitationGraph(typename Publication::id_type const &stem_id) {
+        root = std::make_shared<Node>(stem_id);
     }
 
 // Konstruktor przenoszący i przenoszący operator przypisania. Powinny być
@@ -60,25 +61,24 @@ public:
 // Zwraca identyfikator źródła. Metoda ta powinna być noexcept wtedy i tylko
 // wtedy, gdy metoda Publication::get_id jest noexcept. Zamiast pytajnika należy
 // wpisać stosowne wyrażenie.
-        // https://akrzemi1.wordpress.com/2011/06/10/using-noexcept/
+    // https://akrzemi1.wordpress.com/2011/06/10/using-noexcept/
     //Publication::id_type get_root_id() const noexcept(is_nothrow_move_constructible<T>::value && is_nothrow_move_assignable<T>::value);
-    Publication::id_type get_root_id() const noexcept(?) {
-
+    typename Publication::id_type get_root_id() const noexcept(noexcept(std::declval<Publication>().get_id())) {
+        return root -> publication.get_id();
 
 
     }
 // Zwraca listę identyfikatorów publikacji cytujących publikację o podanym
 // identyfikatorze. Zgłasza wyjątek PublicationNotFound, jeśli dana publikacja
 // nie istnieje.
-    std::vector<Publication::id_type> get_children(Publication::id_type const &id) const {
+    std::vector<typename Publication::id_type> get_children(typename Publication::id_type const &id) const {
         if (!exists(id))
-            throw (PublicationNotFound);
+            throw PublicationNotFound();
 
-        std::vector<Publication::id_type> result;    // weak_ptr<X> WEAK;
-        if (std::shared_ptr myNode = map.at(id).lock()) {  // WEAK.lock() zwraca shared_ptr na obiekt lub false
-            for (auto child : myNode -> children) {     // w sumie to jeśli przeszło exists to chyba nie trzeba tego ifa
-                result.push_back(child -> id);          // z linijki wyżej
-            }
+        std::vector<typename Publication::id_type> result;
+        std::shared_ptr<Node> myNode = map.at(id).lock();
+        for (auto child : myNode -> children) {
+            result.push_back(child -> id);
         }
         return result;
     }
@@ -86,41 +86,85 @@ public:
 // Zwraca listę identyfikatorów publikacji cytowanych przez publikację o podanym
 // identyfikatorze. Zgłasza wyjątek PublicationNotFound, jeśli dana publikacja
 // nie istnieje.
-    std::vector<Publication::id_type> get_parents(Publication::id_type const &id) const {
+    std::vector<typename Publication::id_type> get_parents(typename Publication::id_type const &id) const {
         if (!exists(id))
-            throw (PublicationNotFound);
-        std::vector<Publication::id_type> result;
-        std::shared_ptr myNode = map.at(id);
-        for (auto parent : myNode -> parents) {     // myNode -> parents to zbiór weak_ptr'ów, parent2 to shared_ptr
-            if (std::shared_ptr parent2 = parent.lock())  //  jeśli obiekt wskazywany przez parent istnieje
+            throw PublicationNotFound();
+        std::vector<typename Publication::id_type> result;
+        std::shared_ptr<Node> myNode = map.at(id);
+        for (auto parent : myNode -> parents) {
+            if (std::shared_ptr<Node> parent2 = parent.lock())
                 result.push_back(parent2 -> id);
         }
     }
 
 // Sprawdza, czy publikacja o podanym identyfikatorze istnieje.
-    bool exists(Publication::id_type const &id) const;
+    bool exists(typename Publication::id_type const &id) const {
+        std::weak_ptr<Node> checkNode = map.find(id);
+        if (checkNode == map.end()) return false;
+        if (checkNode.expired()) return false;
+        return true;
+    }
 
 // Zwraca referencję do obiektu reprezentującego publikację o podanym
 // identyfikatorze. Zgłasza wyjątek PublicationNotFound, jeśli żądana publikacja
 // nie istnieje.
-    Publication& operator[](Publication::id_type const &id) const;
+    Publication& operator[](typename Publication::id_type const &id) const {
+        if (!exists(id))
+            throw PublicationNotFound();
+
+        std::shared_ptr<Node> myNode = map.at(id).lock();
+        return myNode -> publication;
+    }
 
 // Tworzy węzeł reprezentujący nową publikację o identyfikatorze id cytującą
 // publikacje o podanym identyfikatorze parent_id lub podanych identyfikatorach
 // parent_ids. Zgłasza wyjątek PublicationAlreadyCreated, jeśli publikacja
 // o identyfikatorze id już istnieje. Zgłasza wyjątek PublicationNotFound, jeśli
 // któryś z wyspecyfikowanych poprzedników nie istnieje.
-    void create(Publication::id_type const &id, Publication::id_type const &parent_id);
-    void create(Publication::id_type const &id, std::vector<Publication::id_type> const &parent_ids);
+    void create(typename Publication::id_type const &id, typename Publication::id_type const &parent_id) {
+        if (exists(id))
+            throw PublicationAlreadyCreated();
+        if (!exists(parent_id))
+            throw PublicationNotFound();
+        // TODO: maybe with add_citation ?
+
+    }
+    void create(typename Publication::id_type const &id, std::vector<typename Publication::id_type> const &parent_ids) {
+        if (exists(id))
+            throw PublicationAlreadyCreated();
+        for (auto parent : parent_ids) {
+            if (!exists(parent))
+                throw PublicationNotFound();
+        }
+        // TODO: maybe with add_citation ?
+
+    }
 
 // Dodaje nową krawędź w grafie cytowań. Zgłasza wyjątek PublicationNotFound,
 // jeśli któraś z podanych publikacji nie istnieje.
-    void add_citation(Publication::id_type const &child_id, Publication::id_type const &parent_id);
+    void add_citation(typename Publication::id_type const &child_id, typename Publication::id_type const &parent_id) {
+        if (!exists(child_id) || !exists(parent_id))
+            throw PublicationNotFound();
+
+    }
 
 // Usuwa publikację o podanym identyfikatorze. Zgłasza wyjątek
 // PublicationNotFound, jeśli żądana publikacja nie istnieje. Zgłasza wyjątek
 // TriedToRemoveRoot przy próbie usunięcia pierwotnej publikacji.
-    void remove(Publication::id_type const &id);
+    void remove(typename Publication::id_type const &id) {
+        if (!exists(id))
+            throw PublicationNotFound();
+        if (id == root -> publication.get_id())
+            throw TriedToRemoveRoot();
+        // TODO: remove this node from every parent, because parents are only ones who got shared_pointers on this node
+        std::shared_ptr<Node> myNode = map.at(id).lock();
+        for (auto parent : myNode -> parents) {
+            if (std::shared_ptr<Node> validParent = parent.lock()) {
+
+            }
+
+        }
+    }
 
 };
 
